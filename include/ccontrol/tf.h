87ee63f4f19c10654f25a41f52525efc732ef2d3
oonamo <onam75hernandez@gmail.com>
2026-07-09 22:02:18 -0700

    fix!: remove dangerous string functions
    The current implementation for strings use unsigned integers for int to
    float conversion, causing overflow and possible undefined behavior

--- include/ccontrol/tf.h
+++ include/ccontrol/tf.h
@@ -31,16 +31,12 @@ control_vector_t PolyCoeffVector_Scratch(ControlHandle *ctx,
 control_vector_t PolyCoeffVector_Persistent(ControlHandle *ctx,
                                             const float *coeffs, size_t size);
 control_vector_t PolyCoeffVector_Cannonicalize(const control_vector_t *v);
-int PolyCoeffVectorToStr(const control_vector_t *coeffs, char *buffer,
-                         size_t buffer_size);
 
 control_vector_t AddCoeffVector(ControlHandle *ctx, const control_vector_t *a,
                                 const control_vector_t *b);
 
 TransferFunction TransferFunctionFromCoeffs(const control_vector_t *num,
                                             const control_vector_t *dem);
-int TransferFunctionToStr(TransferFunction *tf, char *buffer,
-                          size_t buffer_size);
 
 control_vector_t MultiplyPoly(ControlHandle *ctx, control_vector_t *a,
                               control_vector_t *b);
