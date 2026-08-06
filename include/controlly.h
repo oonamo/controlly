924f96e10f104fb9e5ecc206becfd34650f50e73
oonamo <onam75hernandez@gmail.com>
2026-08-06 14:22:25 -0700

    feat: provide umbrella header for modular builds

---
+++ include/controlly.h
@@ -0,0 +1,20 @@
+#ifndef _CONTROLLY_UMBRELLA_H
+#define _CONTROLLY_UMBRELLA_H
+
+#ifdef CONTROLLY_IMPLEMENTATION
+#endif
+
+#include <controlly/arena.h>
+#include <controlly/core.h>
+#include <controlly/matrix.h>
+#include <controlly/statespace.h>
+#include <controlly/tf.h>
+
+/*
+ * ========================================
+ * Controllers
+ * ========================================
+ */
+#include <controlly/controllers/pid.h>
+
+#endif // _CONTROLLY_UMBRELLA_H
